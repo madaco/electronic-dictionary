f56d87d796a0f05bbac779535ab3bd65f0bc11be
madaco <1010504603@qq.com>
2023-12-26 05:20:07 -0500

    update and delete useless

--- client.cpp
+++ client.cpp
@@ -7,79 +7,12 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-#define handle_error_en(en, msg) \
-    do {errno = en; perror(msg); exit(EXIT_FAILURE);} while(0)
-
-char sendBuf[100];
-char recvBuf[100];
-
-pthread_mutex_t mutex;
-
-//线程1负责发送数据,处理对象 sendBuf
-void *client_send(void* socketfd)
-{
-    int *fd = (int *)(socketfd);
-    while(1)
-    {
-        pthread_mutex_lock(&mutex);
-        //memset(&sendBuf, 0, sizeof(sendBuf));
-        printf("输入exit退出\rto server:");
-        scanf("%s",sendBuf);
-        send(*fd, sendBuf, strlen(sendBuf)+1, 0);
-        pthread_mutex_unlock(&mutex);
-
-        sleep(2);
-    }
-
-
-}
-// //线程2负责接收数据,处理对象 recBuf
-// void *client_recv(void* socketfd)
-// {
-//     int *fd = (int *)(socketfd);
-//     while(1)
-//     {
-//         pthread_mutex_lock(&mutex);
-//         memset(&recvBuf, 0, sizeof(recvBuf));
-//         recv(*fd, recvBuf, 100, 0);
-//         printf("recv: %s\n",recvBuf);
-//         pthread_mutex_unlock(&mutex);
-
-//         sleep(2);
-//     }
-// }
-
-
-void pthread(void *socketfd)
-{
-    pthread_t tid_send,tid_recv;
-    int ret;
-
-    pthread_mutex_init(&mutex, NULL);
-
-    ret = pthread_create(&tid_send, NULL, client_send, &socketfd);
-    if (ret)
-    {
-        handle_error_en(ret, "pthread_send failed");
-    }
-
-   
-    // ret = pthread_create(&tid_recv, NULL, client_recv, &socketfd);
-    // if (ret)
-    // {
-    //     handle_error_en(ret, "pthread_recv failed");
-    // }
-
-    pthread_join(tid_send, NULL);
-    // pthread_join(tid_recv, NULL);
-
-    pthread_mutex_destroy(&mutex);
-}
-
 int main()
 {
-    int socket_client;
+    char sendBuf[100];
+    char recvBuf[100];
 
+    int socket_client;
 
     socket_client = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_client == -1)
@@ -116,16 +49,11 @@ int main()
     recv(socket_client, recvBuf, 100, 0);
     printf("Receive server' msg: %s\n",recvBuf);
 
-    //pthread((void *)&socket_client);
     while(1)
     {
-        printf("1---%s---\n",sendBuf);
-
         printf("发送内容(输入exit代表退出): ");
         scanf("%s",sendBuf);
         send(socket_client, sendBuf, strlen(sendBuf)+1, 0); 
-
-        printf("2---%s---\n",sendBuf);
         if(strcmp(sendBuf, "exit") == 0)
         break;
     }
--- server.cpp
+++ server.cpp
@@ -7,76 +7,11 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-#define handle_error_en(en, msg) \
-    do {errno = en; perror(msg); exit(EXIT_FAILURE);} while(0)
-
-char sendBuf[100];
-char recvBuf[100];
-
-pthread_mutex_t mutex;
-
-// //线程1负责发送数据,处理对象 sendBuf
-// void *client_send(void* socketfd)
-// {
-//     int *fd = (int *)(socketfd);
-//     while(1)
-//     {
-//         pthread_mutex_lock(&mutex);
-//         memset(&sendBuf, 0, sizeof(sendBuf));
-//         printf("输入exit退出\rto server:");
-//         scanf("%s",sendBuf);
-//         send(*fd, sendBuf, strlen(sendBuf)+1, 0);
-//         pthread_mutex_unlock(&mutex);
-
-//         sleep(2);
-//     }
-
-
-// }
-//线程2负责接收数据,处理对象 recBuf
-void *client_recv(void* socketfd)
-{
-    int *fd = (int *)(socketfd);
-    while(1)
-    {
-        pthread_mutex_lock(&mutex);
-        //memset(&recvBuf, 0, sizeof(recvBuf));
-        recv(*fd, recvBuf, 100, 0);
-        printf("recv: %s\n",recvBuf);
-        pthread_mutex_unlock(&mutex);
-
-        sleep(2);
-    }
-}
-
-
-void pthread(void *socketfd)
-{
-    pthread_t tid_send,tid_recv;
-    int ret;
-
-    pthread_mutex_init(&mutex, NULL);
-
-    // ret = pthread_create(&tid_send, NULL, client_send, &socketfd);
-    // if (ret)
-    // {
-    //     handle_error_en(ret, "pthread_send failed");
-    // }
-
-    ret = pthread_create(&tid_recv, NULL, client_recv, &socketfd);
-    if (ret)
-    {
-        handle_error_en(ret, "pthread_recv failed");
-    }
-
-    // pthread_join(tid_send, NULL);
-    pthread_join(tid_recv, NULL);
-
-    pthread_mutex_destroy(&mutex);
-}
-
 int main()
 {
+    char sendBuf[100];
+    char recvBuf[100];
+
     int socket_serv;
     sockaddr_in addr_serv;
     int res;
@@ -128,14 +63,10 @@ int main()
         recv(socket_cli, recvBuf, 100, 0);
         printf("Receive client's msg: %s\n",recvBuf);
 
-        //pthread((void *)&socket_cli);
         while(1)
         {
-            printf("1---%s---\n",recvBuf);
-
             recv(socket_cli, recvBuf, 100, 0);
-
-            printf("2-Receive client's msg: %s\n",recvBuf);
+            printf("Receive client's msg: %s\n",recvBuf);
             if(strcmp(recvBuf, "exit") == 0)
             break;
         }
